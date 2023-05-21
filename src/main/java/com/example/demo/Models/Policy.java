ae12af894048ae2f59a95bea86506e5722147766
alghaffri <isami.abdullah@gmail.com>
2023-05-21 11:04:08 +0400

    define Policy class

--- src/main/java/com/example/demo/Models/Policy.java
+++ src/main/java/com/example/demo/Models/Policy.java
@@ -5,11 +5,33 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
 @Entity
 @Data
 public class Policy {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String country;
+    private String region;
+    private String topic;
+    private String details;
+
+    // No-args constructor, required by JPA
+    public Policy() {
+    }
+
+    public Policy(String country, String region, String topic, String details) {
+        this.country = country;
+        this.region = region;
+        this.topic = topic;
+        this.details = details;
+    }
 
 }

569f9fc61f91651085f4b2c06f627a263f4664a8
alghaffri <isami.abdullah@gmail.com>
2023-05-21 15:49:44 +0400

    Update NewsService

--- src/main/java/com/example/demo/Service/NewsService.java
+++ src/main/java/com/example/demo/Service/NewsService.java
@@ -1,4 +1,40 @@
 package com.example.demo.Service;
 
+import com.example.demo.Models.News;
+import com.example.demo.Repository.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class NewsService {
+    private final NewsRepository newsRepository;
+
+    @Autowired
+    public NewsService(NewsRepository newsRepository) {
+        this.newsRepository = newsRepository;
+    }
+
+    public News addNews(News news) {
+        return newsRepository.save(news);
+    }
+
+    public List<News> getAllNews() {
+        return newsRepository.findAll();
+    }
+
+    public Optional<News> getNewsById(Long id) {
+        return newsRepository.findById(id);
+    }
+
+    public News updateNews(News news) {
+        return newsRepository.save(news);
+    }
+
+    public void deleteNews(Long id) {
+        newsRepository.deleteById(id);
+    }
+
 }

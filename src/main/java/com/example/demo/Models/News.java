b47f92ea5341ec70154f42aeb2de1226f5166296
alghaffri <isami.abdullah@gmail.com>
2023-05-21 10:54:33 +0400

    add classes in Model Pakege

---
+++ src/main/java/com/example/demo/Models/News.java
@@ -0,0 +1,15 @@
+package com.example.demo.Models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@Entity
+@Data
+public class News {
+
+}

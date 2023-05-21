1dc94ab31cdee6835796d7c24d9268dc07e9f7e8
alghaffri <isami.abdullah@gmail.com>
2023-05-21 11:54:09 +0400

    add maping to PolicyController

--- src/main/java/com/example/demo/Controller/PolicyController.java
+++ src/main/java/com/example/demo/Controller/PolicyController.java
@@ -1,4 +1,30 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Models.Policy;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/policies")
 public class PolicyController {
-}
+    private final PolicyService policyService;
+
+    @Autowired
+    public PolicyController(PolicyService policyService) {
+        this.policyService = policyService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Policy> addPolicy(@RequestBody Policy policy) {
+        Policy newPolicy = policyService.addPolicy(policy);
+        return new ResponseEntity<>(newPolicy, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Policy>> getAllPolicies() {
+        List<Policy> policies = policyService.getAllPolicies();
+        return new ResponseEntity<>(policies, HttpStatus.OK);
+    }
+

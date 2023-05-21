a6015846a727b8d1ce8ae8aaf0adb7d63871f55c
alghaffri <isami.abdullah@gmail.com>
2023-05-21 11:47:33 +0400

    Create PolicyController

--- src/main/java/com/example/demo/Repository/PolicyRepository.java
+++ src/main/java/com/example/demo/Repository/PolicyRepository.java
@@ -3,6 +3,11 @@ package com.example.demo.Repository;
 import com.example.demo.Models.Policy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PolicyRepository extends JpaRepository<Policy, Long> {
+    List<Policy> findByCountry(String country);
+    List<Policy> findByRegion(String region);
+    List<Policy> findByTopic(String topic);
 
 }

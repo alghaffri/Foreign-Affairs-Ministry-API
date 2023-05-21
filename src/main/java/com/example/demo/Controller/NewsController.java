04976a1de3eff3f865ee02948bc065ea0bd3f624
alghaffri <isami.abdullah@gmail.com>
2023-05-21 16:06:51 +0400

    Update NewsController

--- src/main/java/com/example/demo/Controller/NewsController.java
+++ src/main/java/com/example/demo/Controller/NewsController.java
@@ -1,7 +1,12 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Models.News;
 import com.example.demo.Service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +19,10 @@ public class NewsController {
     public NewsController(NewsService newsService) {
         this.newsService = newsService;
     }
-
+    @PostMapping
+    public ResponseEntity<News> addNews(@RequestBody News news) {
+        News newNews = newsService.addNews(news);
+        return new ResponseEntity<>(newNews, HttpStatus.CREATED);
+    }
 
 }

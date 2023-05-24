31349dc79558eb331b3dda47f82fb78f86365c92
alghaffri <isami.abdullah@gmail.com>
2023-05-24 10:49:41 +0400

    Update

--- src/main/java/com/example/demo/Controller/NewsController.java
+++ src/main/java/com/example/demo/Controller/NewsController.java
@@ -62,6 +62,7 @@ public class NewsController {
         List<News> news = newsService.getNewsByTitle(title);
         return new ResponseEntity<>(news, HttpStatus.OK);
     }
+    
 
 
 
--- src/main/java/com/example/demo/Models/BaseEntity.java
+++ src/main/java/com/example/demo/Models/BaseEntity.java
@@ -14,5 +14,4 @@ public class BaseEntity {
     Date updatedDate;
     Boolean isActive;
 
-    
 }
